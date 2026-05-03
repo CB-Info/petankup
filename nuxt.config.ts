3c225f8d6361a39f07c37610cf1f0471125a82fe
Clément <clement.bilger25@gmail.com>
2026-05-03 11:55:05 +0200

    chore: configure nuxt 4 spa with nuxt ui, pinia and vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  ssr: false,
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
 });
